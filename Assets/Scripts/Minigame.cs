01fb230eab7de235fdc3daa4fb11ab8a1a2c0458
c9charles9 <angcanan.charles@gmail.com>
2020-02-11 20:06:39 +0800

    Moved Scripts into project folder

---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Minigame
+{
+    public int bet;
+    [HideInInspector] public int pool, rewards;
+    public int correctNeededInSet = 3, setCount = 5;
+    public float setChance = 30;
+
+    [HideInInspector] public float chance;
+    [HideInInspector] public int currentCorrectInSet, streak = 0;
+
+    [HideInInspector] public bool playerBetted, guessed, claimedReward;
+    [HideInInspector] public bool hasPool;
+}
